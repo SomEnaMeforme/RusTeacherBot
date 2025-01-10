079924f99dd00c4f6b2bc9ca8d936944b4703620
SomEnaMeforme <rushkova909@gmail.com>
2025-01-10 05:58:29 +0500

    Change neiro for sst

--- audio_transcriber.py
+++ audio_transcriber.py
@@ -1,13 +1,16 @@
-import io
 import logging
-from typing import BinaryIO, Tuple
-import librosa
+from typing import  Tuple
 import soundfile as sf
 import speech_recognition as sr
 from aiogram import types
+import whisper
+import pathlib
+import os
+import torch
 from aiogram.client.bot import Bot
 
 logger = logging.getLogger(__name__)
+FRAMES_PER_SECOND = 44100
 
 
 class AudioTranscriber:
@@ -17,31 +20,50 @@ class AudioTranscriber:
         self.language = language
         self.sample_rate = sample_rate
         self.recognizer = sr.Recognizer()
+        self.modelSST = whisper.load_model("large")
+        self.modelTTS, _ = torch.hub.load(
+            'snakers4/silero-models',
+            'silero_tts',
+            language='ru',
+            speaker='v3_1_ru',
+            trust_repo=True
+        )
 
-    async def load_audio_from_message(self, message: types.Message) -> BinaryIO:
+    async def load_audio_from_message(self, message: types.Message) -> str:
         try:
-            with io.BytesIO() as voice_message:
-                file_info = await self.bot.get_file(message.voice.file_id)
-                await self.bot.download_file(file_info.file_path, destination=voice_message)
-                sound_data, sample_rate = librosa.load(voice_message, sr=self.sample_rate)
-            
-            voice_file = io.BytesIO()
-            sf.write(voice_file, sound_data, sample_rate, format='WAV', subtype='PCM_16')
-            voice_file.seek(0)
-            return voice_file
+            file_id = message.voice.file_id
+            file = await self.bot.get_file(file_id)
+            file_extension = os.path.splitext(file.file_path)[1]
+
+            voice_file_name = f"{file_id}{file_extension}"
+            open(voice_file_name, 'a').close()
+            await self.bot.download_file(file.file_path, destination=voice_file_name)
+
+            wav_path = self.convert_to_wav(voice_file_name, file_id)
+
+            os.remove(voice_file_name)
+
+            return wav_path
         except Exception as e:
             logger.error(f"Error loading audio: {str(e)}")
             raise
 
-    def transcribe_audio(self, voice: BinaryIO) -> Tuple[str, bool]:
+    def convert_to_wav(self, audio_file_name: str, file_id: str):
+        data, samplerate = sf.read(audio_file_name)
+        wav_path = f"{file_id}.wav"
+        open(wav_path, 'a').close()
+        sf.write(wav_path, data, samplerate, format='WAV', subtype='PCM_16')
+        dir = pathlib.Path().resolve()
+        return os.path.join(dir, wav_path)
+
+    def transcribe_audio(self, voice_file: str) -> Tuple[str, bool]:
         """
         Transcribe audio to text
         Returns: (transcribed_text: str, success: bool)
         """
         try:
-            with sr.AudioFile(voice) as source:
-                audio = self.recognizer.record(source)
-                text = self.recognizer.recognize_google(audio, language=self.language)
+                text: str = self.modelSST.transcribe(voice_file)['text']
+                os.remove(voice_file)
                 return text, True
         except sr.UnknownValueError:
             return "Не удалось распознать речь", False
